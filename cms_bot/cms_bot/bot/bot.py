9018bf8a091aedfa90b3b4887db2967014c30e53
Charles de Freitas <me@chasbob.dev>
2020-10-01 15:51:55 +0100

    Clean up bot

--- cms_bot/cms_bot/bot/bot.py
+++ cms_bot/cms_bot/bot/bot.py
@@ -1,10 +1,12 @@
 import logging
+import asyncio
 from typing import Optional
 
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 
-from cms_bot.util import config
+from ..util import config
+from .updater import UpdateCog
 from sqlalchemy.orm import Session
 
 from cms_bot import crud
@@ -17,22 +19,6 @@ command_prefix = config.config['discord']['command_prefix']
 bot: commands.Bot = commands.Bot(command_prefix='-', pass_context=True)
 
 
-class DefaultChannelConverter(commands.Converter):
-    async def convert(self, ctx: commands.Context, argument):
-        if int(argument) == 0:
-            return ctx.channel
-        else:
-            return ctx.guild.get_channel(int(argument))
-
-
-class UserConverter(commands.Converter):
-    async def convert(self, ctx, argument):
-        db = SessionLocal()
-        user = crud.User.get_or_create(db, id=int(ctx.author.id))
-        db.close()
-        return user
-
-
 def start():
     global bot
     logger.info("Starting bot...")
@@ -43,75 +29,44 @@ def start():
 async def create_message(
     ctx: commands.Context,
     *,
-    channel: DefaultChannelConverter = None,
+    channel: discord.TextChannel = None,
 ):
-    logger.info("create...")
-    db = SessionLocal()
-    user = crud.User.get_or_create(db, id=str(ctx.author.id))
-    logger.info(f"create -> user={user}")
     if channel == None:
         channel = ctx.channel
-    logger.info(channel)
-    msg: discord.Message = await channel.send(content='.')
+    db = SessionLocal()
+    guild = crud.Guild.get_or_create(db, id=str(ctx.guild.id))
+    user = crud.User.get_or_create(db, id=str(ctx.author.id))
+    content = f"by: {user.username}"
+    logger.info(
+        f"creating user={ctx.author}, channel={channel}, content={content}")
+    msg: discord.Message = await channel.send(content=content)
     crud.Message.create(db,
                         channel=str(channel.id),
                         id=str(msg.id),
                         user_id=str(user.id),
                         user=user,
+                        guild_id=guild.id,
+                        guild=guild,
                         name='.',
-                        content='.')
-    await ctx.author.send(f"created message {msg.jump_url}")
+                        content=content)
     db.close()
-
-
-@bot.command(name="check")
-async def check_message(ctx: commands.Context, message: discord.Message):
-    db = SessionLocal()
-    post = crud.Message.get(db, message=message.id)
-    logger.info(f"post={post}")
-    logger.info(message)
-    await ctx.send(f"id={message.id}, link={message.jump_url}")
-    db.close()
-
-
-@bot.command(name="users")
-async def list_users(ctx: commands.Context):
-    db = SessionLocal()
-    users = crud.User.list()
-    for user in users:
-        logger.info(user)
-        await ctx.send(str(user))
-
-
-@bot.command(name="list")
-async def list_posts(ctx: commands.Context):
-    db = SessionLocal()
-    user = crud.User.get_or_create(db, id=str(ctx.author.id))
-    posts = crud.Message.list(db, user=user)
-    for post in posts:
-        ch = discord.TextChannel = ctx.guild.get_channel(int(post.channel))
-        message: discord.Message = await ch.fetch_message(int(post.id))
-        await ctx.send(message.jump_url)
-    db.close()
-
-
-@bot.command(name="edit")
-async def edit_post(ctx: commands.Context, message_id: int, content: str):
-    db = SessionLocal()
-    user = crud.User.get_or_create(db, id=int(ctx.author.id))
-    post = crud.Message.get(db, message=message_id)
-    if user != post.user:
-        await ctx.send(f"Can't edit someone else's message!")
-    logger.info(f"post={post}")
-    message: discord.Message = await ctx.guild.get_channel(int(
-        post.channel)).fetch_message(int(post.message))
-    await message.edit(content=content)
-    logger.info(message)
 
 
 @bot.event
 async def on_ready():
     guild: discord.Guild
+    db = SessionLocal()
     for guild in bot.guilds:
         logger.info(f"Logged into {guild}")
+        g = crud.Guild.get_or_create(db, id=str(guild.id))
+        logger.info(g.__dict__)
+        g.name = guild.name
     logger.info("Bot online")
+    db.commit()
+    db.close()
+    bot.add_cog(UpdateCog(bot=bot))
+
+
+@bot.after_invoke
+async def after_invoke(ctx):
+    await ctx.message.delete()
